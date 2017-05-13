40b65c8211b50bb46468393596fa8c9c835075b3
Andreas P <andreaspreikschat@posteo.de>
2017-05-13 11:40:37 +0200

    Refactor: extract code to reduce method complexity

--- coreAPI/src/java/net/java/games/input/DefaultControllerEnvironment.java
+++ coreAPI/src/java/net/java/games/input/DefaultControllerEnvironment.java
@@ -129,28 +129,8 @@ class DefaultControllerEnvironment extends ControllerEnvironment {
                     return null;
                 }
             });
-            //Check the properties for specified controller classes
-            String pluginClasses = getPrivilegedProperty("jinput.plugins", "") + " " + getPrivilegedProperty("net.java.games.input.plugins", "");
-			if(!getPrivilegedProperty("jinput.useDefaultPlugin", "true").toLowerCase().trim().equals("false") && !getPrivilegedProperty("net.java.games.input.useDefaultPlugin", "true").toLowerCase().trim().equals("false")) {
-				String osName = getPrivilegedProperty("os.name", "").trim();
-				if(osName.equals("Linux")) {
-					pluginClasses = pluginClasses + " net.java.games.input.LinuxEnvironmentPlugin";
-				} else if(osName.equals("Mac OS X")) {
-					pluginClasses = pluginClasses + " net.java.games.input.OSXEnvironmentPlugin";
-				} else  if(osName.equals("Windows XP") || osName.equals("Windows Vista") || osName.equals("Windows 7") || osName.equals("Windows 8") || osName.equals("Windows 8.1") || osName.equals("Windows 10")) {
-					pluginClasses = pluginClasses + " net.java.games.input.DirectAndRawInputEnvironmentPlugin";
-				} else if(osName.equals("Windows 98") || osName.equals("Windows 2000")) {
-					pluginClasses = pluginClasses + " net.java.games.input.DirectInputEnvironmentPlugin";
-				} else if (osName.startsWith("Windows")) {
-					log.warning("Found unknown Windows version: " + osName);
-					log.warning("Attempting to use default windows plug-in.");
-					pluginClasses = pluginClasses + " net.java.games.input.DirectAndRawInputEnvironmentPlugin";
-				} else {
-					log.warning("Trying to use default plugin, OS name " + osName +" not recognised");
-				}
-			}
 
-			StringTokenizer pluginClassTok = new StringTokenizer(pluginClasses, " \t\n\r\f,;:");
+			StringTokenizer pluginClassTok = new StringTokenizer(detectPluginClasses(), " \t\n\r\f,;:");
 			while(pluginClassTok.hasMoreTokens()) {
 				String className = pluginClassTok.nextToken();					
 				try {
@@ -179,6 +159,30 @@ class DefaultControllerEnvironment extends ControllerEnvironment {
         }
         return ret;
     }
+
+	private String detectPluginClasses() {
+		//Check the properties for specified controller classes
+		String pluginClasses = getPrivilegedProperty("jinput.plugins", "") + " " + getPrivilegedProperty("net.java.games.input.plugins", "");
+		if(!getPrivilegedProperty("jinput.useDefaultPlugin", "true").toLowerCase().trim().equals("false") && !getPrivilegedProperty("net.java.games.input.useDefaultPlugin", "true").toLowerCase().trim().equals("false")) {
+			String osName = getPrivilegedProperty("os.name", "").trim();
+			if(osName.equals("Linux")) {
+				pluginClasses = pluginClasses + " net.java.games.input.LinuxEnvironmentPlugin";
+			} else if(osName.equals("Mac OS X")) {
+				pluginClasses = pluginClasses + " net.java.games.input.OSXEnvironmentPlugin";
+			} else  if(osName.equals("Windows XP") || osName.equals("Windows Vista") || osName.equals("Windows 7") || osName.equals("Windows 8") || osName.equals("Windows 8.1") || osName.equals("Windows 10")) {
+				pluginClasses = pluginClasses + " net.java.games.input.DirectAndRawInputEnvironmentPlugin";
+			} else if(osName.equals("Windows 98") || osName.equals("Windows 2000")) {
+				pluginClasses = pluginClasses + " net.java.games.input.DirectInputEnvironmentPlugin";
+			} else if (osName.startsWith("Windows")) {
+				log.warning("Found unknown Windows version: " + osName);
+				log.warning("Attempting to use default windows plug-in.");
+				pluginClasses = pluginClasses + " net.java.games.input.DirectAndRawInputEnvironmentPlugin";
+			} else {
+				log.warning("Trying to use default plugin, OS name " + osName +" not recognised");
+			}
+		}
+		return pluginClasses;
+	}
     
     /* This is jeff's new plugin code using Jeff's Plugin manager */
     private void scanControllers() {
